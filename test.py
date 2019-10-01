0b01fe95da706232af00e772f96d5c9e172ddafd
A201600321 <A201600321@du.edu.om>
2019-10-01 13:57:38 +0400

    added file 1

---
+++ test.py
@@ -0,0 +1,8 @@
+a=input("enter number 1: ")
+b=input("enter number 2: ")
+c=  int(a) + int(b)
+print ("The sum is", c ," and the addition is done.")
+a=input("enter firstname : ")
+b=input("enter secondname : ")
+c=  a +" "+b
+print("you are, " , c)
---
+++ test1.py
@@ -0,0 +1,11 @@
+def add(a,b): 
+    if a>b:
+        print("a is greater than b ")
+    elif a==b:
+        print("b is equal a ")
+    else:
+        print("b is greater than a ")
+        
+        
+
+add(8,8)
