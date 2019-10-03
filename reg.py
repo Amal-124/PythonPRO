6239cfd9d33ea11c7d69c23d90d0665525b51940
A201600321 <A201600321@du.edu.om>
2019-10-03 13:52:33 +0400

    m

--- reg.py
+++ reg.py
@@ -7,4 +7,7 @@ def reg(name, gba):
 ### outside
 reg('amal', 98)
 student=['amal', 'ahmad', 'anwar', 50 , 70] #list
+student.remove('amal') #remove by name
+del student[2] #remove by index
+student.append("salim") #to add to the list
 print(student)
